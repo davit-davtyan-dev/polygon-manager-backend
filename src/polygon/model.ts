c0a184e12bc4f5c1a53b7692a0ade4fda9090214
Davit Davtyan <davit.davtyan.dev@gmail.com>
2025-04-10 23:32:18 +0400

    Fixed points type and validation

--- src/polygon/model.ts
+++ src/polygon/model.ts
@@ -10,10 +10,25 @@ export type Polygon = {
 const PolygonSchema = new mongoose.Schema<Polygon>({
   name: { type: String, required: [true, "Please provide name"] },
   points: {
-    type: [Number],
+    type: [[Number]],
     validate: {
-      validator: (value: Array<number>) => value.length === 2,
-      message: () => "Points must be an array with 2 numbers",
+      validator: (value: Array<Array<number>>) =>
+        value.length >= 3 &&
+        value.every((point) => Array.isArray(point) && point.length === 2),
+      message: (props) => {
+        const value = props.value as Array<Array<number>>;
+        if (value.length < 3) {
+          return "Please provide at least 3 points";
+        }
+
+        if (
+          value.some((point) => !Array.isArray(point) || point.length !== 2)
+        ) {
+          return "Each point must be an array with 2 numbers";
+        }
+
+        return "Please provide valid points";
+      },
     },
   },
 });
